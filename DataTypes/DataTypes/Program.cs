263bd4fdeb6bd27d579760ae587e123d9a322ca3
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-03 11:46:08 +0300

    ij

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Threading.Channels;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----test----");
+
+            Console.WriteLine("Hello, World!");
+
+            //string e tähemärkide jada, mis on defineeritud andmetüübiga string
+            //andmetüüp toetab ainult true ja false väärtusi
+            //myString on muutuja, mis on defineeritud andmetüübiga string
+            //saan sisestada kõike, mis on klaviatuuril
+            string myString = "Hello, World";
+            //kirjutage cw ja vajutage tab, et genereerida Console.Writeline
+            Console.WriteLine(myString);
+            string one = "1";
+            string two = " 2";
+            Console.WriteLine(one + two);
+
+            Console.WriteLine("----Täisarvud----");
+            //int on täisarvuline andmetüüp, mis toetab ainult täisarve
+            //see on 32-bitine täisarvuline andmetüüp
+            int myInt1 = 2;
+            int myInt2 = 100;
+            int myInt3 = 310;
+            int myInt4 = 111;
+            Console.WriteLine(myInt1);
+            //kui kaks int tüüpi muutujat liita, siis liidetakse nende väärtused kokku
+            Console.WriteLine(myInt1 * myInt2 - myInt3 + myInt4);
+
+            Console.WriteLine("----bool----");
+            //see on andmetüüp, mis toetab ainult true ja false väärtusi
+            bool myBool = true;
+            Console.WriteLine(myBool);
+
+            Console.WriteLine("----Ujukomaarvud----");
+            //double on ujukomaarvuline andmetüüp, mis toetab ainult ujukomaarve
+            double myDouble = 5.5;
+            double secondDouble = 10.5;
+            Console.WriteLine(myDouble);
+            //kui kaks double tüüpi muutujat liita, siis liidetakse nende väärtused kokku
+            Console.WriteLine(myDouble + secondDouble);
+
+            //float on ujukomaarvuline andmetüüp, mis toetab ainult ujukomaarve
+            Console.WriteLine("----Float----");
+            float myFloat = 5.5f;
+            float secondFloat = 10.5f;
+            Console.WriteLine(myFloat);
+            Console.WriteLine(myFloat + secondFloat);
+
+            Console.WriteLine("----Decimal----");
+            //decimal on ujukomaarvuline andmetüüp, mis toetab ainult ujukomaarve
+            decimal myDecimal = 5.5m;
+            decimal secondDecimal = 10.5m;
+            Console.WriteLine(myDecimal);
+            Console.WriteLine(myDecimal + secondDecimal);
+
+            Console.WriteLine("----Byte----");
+            //byte on täisarvuline andmetüüp, mis toetab
+            //ainult täisarve vahemikus 0 kuni 255
+            //see on 8-bitine täisarvuline andmetüüp
+            byte myByte = 5;
+            Console.WriteLine(myByte);
+
+            Console.WriteLine("----Long----");
+            //long on täisarvuline andmetüüp, mis toetab ainult täisarve
+            //selle maksimaalne väärtus on 9,223,372,036,854,775,807
+            //see on 64-bitine täisarvuline andmetüüp
+            long myLong = 5L;
+            long secondLong = 10L;
+            Console.WriteLine(myLong);
+            Console.WriteLine(myLong + secondLong);
+
+            Console.WriteLine("----Short----");
+            //short on täisarvuline andmetüüp, mis toetab ainult täisare
+            //selle maksimaalne väärtus on 32,767
+            //see on 16-bitine täisarvuline andmetüüp
+            short myShort = 3728;
+            short secondShort = 1829;
+            Console.WriteLine(myShort);
+            Console.WriteLine(myShort + secondShort);
+        }
+    }
+}
---
+++ intro/intro/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace intro   //See on projekti nimetus
+{
+    internal class Program  //see on klassi nimetus
+    {
+        //meetodit ei saa panna namespace'i, kuna see on klassi sees
+        static void Main(string[] args)  //See on peamine meetod, mis käivitatakse programmi käivitamisel
+        {
+            Console.WriteLine("Hello, World!");  //see on konsooli väljund, mis prindib "Hello, World" ekraanile
+            //kui tahad topelt rida teha, siis vajuta Ctrl + D
+            //kui tahad kustutada rida, siis vajuta Ctrl + X
+        }
+        
+        //breakpointi saab panna koodirea vasakule poolele,
+        //kus on hall ruut. Kui programm jõuab
+        //selle koodirea juurde, peatub see ja võimaldab teil
+        //uurida muutujate väärtusi ja programmi olekut
+    }
+}
