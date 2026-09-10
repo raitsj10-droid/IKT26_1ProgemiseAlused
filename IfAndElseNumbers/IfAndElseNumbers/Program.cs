afb0903f91ed0b4aae871850adc32a2a2a31e25d
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-10 10:51:34 +0300

    IfAndElseNumbers

---
+++ IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //konsoolis sisestame numbri
+            //see number salvestatakse string andmetüüpi
+            //toimub kontroll, kas sisestatud väärtus on number
+            //kasutada if ja else lauseid
+
+            Console.WriteLine("Sisesta number:");
+
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                Console.WriteLine("Sisestasid numbri");
+                Console.WriteLine(number);
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+}
